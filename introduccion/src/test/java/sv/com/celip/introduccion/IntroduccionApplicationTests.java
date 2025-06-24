7cbfc7856938947ac309476a3c3fc99a6114dfed
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2025-06-23 21:30:12 -0600

    Ejercicios propuestos en guia de introduccion

---
+++ introduccion/src/test/java/sv/com/celip/introduccion/IntroduccionApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.celip.introduccion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroduccionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

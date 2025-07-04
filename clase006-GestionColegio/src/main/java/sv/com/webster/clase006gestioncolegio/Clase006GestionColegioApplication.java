c7287b1cce809d4591934a301b3edbaf39bd3c3e
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2025-07-03 20:32:30 -0600

    Base project added

---
+++ clase006-GestionColegio/src/main/java/sv/com/webster/clase006gestioncolegio/Clase006GestionColegioApplication.java
@@ -0,0 +1,13 @@
+package sv.com.webster.clase006gestioncolegio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase006GestionColegioApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Clase006GestionColegioApplication.class, args);
+  }
+
+}
---
+++ clase006-GestionColegio/src/test/java/sv/com/webster/clase006gestioncolegio/Clase006GestionColegioApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.com.webster.clase006gestioncolegio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase006GestionColegioApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

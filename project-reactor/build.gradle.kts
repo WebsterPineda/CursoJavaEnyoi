59da580a6e9fbbb5114666718fd0ba768c442764
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2025-08-11 21:00:17 -0600

    Loaded base project

---
+++ project-reactor/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "sv.com.webster"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    implementation("io.projectreactor:reactor-core:3.7.8")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}

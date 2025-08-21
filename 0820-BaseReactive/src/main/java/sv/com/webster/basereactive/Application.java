4d99ff613863e3e0991c8eb50f713f978238846b
Webster Pineda <38181952+WebsterPineda@users.noreply.github.com>
2025-08-21 11:31:23 -0600

    Added initial project with configuration

---
+++ 0820-BaseReactive/src/main/java/sv/com/webster/basereactive/Application.java
@@ -0,0 +1,13 @@
+package sv.com.webster.basereactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
